e6ae3faea891015630a9d4ea08da2e14d7bd2813
Khushhhi <108921822+Khushhhi@users.noreply.github.com>
2024-02-01 18:23:51 +0530

    app formed

---
+++ ihearu/ihearu/ihearuApp.swift
@@ -0,0 +1,17 @@
+//
+//  ihearuApp.swift
+//  ihearu
+//
+//  Created by khushi mittal on 01/02/24.
+//
+
+import SwiftUI
+
+@main
+struct ihearuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
