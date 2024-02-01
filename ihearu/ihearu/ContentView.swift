28d7379fadea393519de15414530e32a97bf073f
Khushhhi <108921822+Khushhhi@users.noreply.github.com>
2024-02-01 19:20:41 +0530

    listener button added

--- ihearu/ihearu/ContentView.swift
+++ ihearu/ihearu/ContentView.swift
@@ -8,13 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isAnimating = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        NavigationView {
+            
+            Button(action: {
+                isAnimating.toggle()
+            }, label: {
+                VStack(spacing: 16){
+//                    Image(systemName: "headphones")
+//                        .foregroundColor(.white)
+                    Text(isAnimating ? "🐣" : "🥚")
+                        .font(.system(size: 100))
+                    Text("Touch Me")
+                        .fontWeight(.bold)
+                        .font(.headline)
+                        .foregroundStyle(Color.white)
+                }
+                .frame(width: 200, height: 200)
+                .padding()
+                .background(.black)
+                .clipShape(Circle())
+                
+                .overlay(
+                    Circle()
+                        .stroke(isAnimating ? Color.green : Color.blue, lineWidth: 2)
+                    
+                        .scaleEffect(isAnimating ? 2 : 1) // Pulsing effect
+                        .opacity(isAnimating ? 0 : 1) // Fade out effect when animating
+                        .animation(.easeOut(duration: 0.6).repeatForever(autoreverses: true), value: isAnimating)
+                )
+                
+            })
+            
         }
+        .navigationBarTitle("ihearu")
+//        .foregroundColor(.black)
         .padding()
     }
 }
